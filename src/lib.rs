a4e59622b1f23c1a21cec18b26d1bc2587e97635
Ossi Herrala <oherrala@iki.fi>
2016-10-28 20:10:04 +0300

    Queries to Cymru can return multiple TXT results
    With this change we query "origin" once after that query "asn" for
    every unique AS number found in "origin" responses.
    
    cymru_ip2asn() function now returns vector of results or error if none
    are found.

--- src/lib.rs
+++ src/lib.rs
@@ -53,39 +53,60 @@ struct CymruOrigin {
 
 /// Query Cymru's IP-to-ASN mapping service
 ///
-/// This function produces two DNS queries over network.
-///
-pub fn cymru_ip2asn(ip: IpAddr) -> Result<CymruIP2ASN, String> {
-    let origin: CymruOrigin = try!(cymru_origin(ip));
-    let asn: CymruASN = try!(cymru_asn(origin.as_number));
-    let result = CymruIP2ASN {
-        ip_addr: ip.to_string(),
-        bgp_prefix: origin.bgp_prefix,
-        as_number: origin.as_number,
-        as_name: asn.as_name,
-        country_code: origin.country_code,
-        registry: origin.registry,
-        allocated: origin.allocated.map(|s| s.to_string()),
-    };
-    Ok(result)
+pub fn cymru_ip2asn(ip: IpAddr) -> Result<Vec<CymruIP2ASN>, String> {
+    let origins: Vec<CymruOrigin> = try!(cymru_origin(ip));
+    let mut results: Vec<CymruIP2ASN> = Vec::with_capacity(origins.len());
+
+    'origins: for origin in origins {
+        for result in &results {
+            if origin.as_number == result.as_number {
+                continue 'origins;
+            }
+        }
+
+        let asn: Vec<CymruASN> = try!(cymru_asn(origin.as_number));
+
+        let result = CymruIP2ASN {
+            ip_addr: ip.to_string(),
+            bgp_prefix: origin.bgp_prefix,
+            as_number: origin.as_number,
+            as_name: asn[0].as_name.to_string(),
+            country_code: origin.country_code,
+            registry: origin.registry,
+            allocated: origin.allocated.map(|s| s.to_string()),
+        };
+        results.push(result);
+    }
+
+    if results.is_empty() {
+        return Err("No results found".to_string());
+    }
+
+    Ok(results)
 }
 
 
 /// Resolve information about AS number
 ///
-fn cymru_asn(asn: u32) -> Result<CymruASN, String> {
+fn cymru_asn(asn: u32) -> Result<Vec<CymruASN>, String> {
     let query = format!("AS{}.asn.cymru.com", asn.to_string());
 
     match resolve_txt(&query) {
         Err(err) => Err(err.to_string()),
-        Ok(records) => parse_cymru_asn(records),
+        Ok(records) => {
+            let results = parse_cymru_asn(records);
+            if results.is_empty() {
+                return Err("No results found".to_string());
+            }
+            return Ok(results);
+        }
     }
 }
 
 
 /// Resolve information about IP address
 ///
-fn cymru_origin(ip: IpAddr) -> Result<CymruOrigin, String> {
+fn cymru_origin(ip: IpAddr) -> Result<Vec<CymruOrigin>, String> {
     let query = match ip {
         IpAddr::V4(ipv4) => {
             let o = ipv4.octets();
@@ -99,7 +120,13 @@ fn cymru_origin(ip: IpAddr) -> Result<CymruOrigin, String> {
 
     match resolve_txt(&query) {
         Err(err) => Err(err.to_string()),
-        Ok(records) => parse_cymru_origin(records),
+        Ok(records) => {
+            let results = parse_cymru_origin(records);
+            if results.is_empty() {
+                return Err("No results found".to_string());
+            }
+            return Ok(results);
+        }
     }
 }
 
@@ -112,27 +139,32 @@ fn cymru_origin(ip: IpAddr) -> Result<CymruOrigin, String> {
 ///
 /// taken from https://www.team-cymru.org/IP-ASN-mapping.html#dns
 ///
-fn parse_cymru_asn(records: Vec<String>) -> Result<CymruASN, String> {
-    if records.len() != 1 {
-        return Err("Invalid number of records found".to_string());
+fn parse_cymru_asn(records: Vec<String>) -> Vec<CymruASN> {
+    let mut results = Vec::with_capacity(records.len());
+
+    for record in records {
+        let fields: Vec<&str> = record.split("|").map(str::trim).collect();
+        let as_number: u32 = match fields[0].parse() {
+            Err(_) => continue,
+            Ok(n) => n,
+        };
+
+        let result = CymruASN {
+            as_number: as_number,
+            country_code: fields[1].to_string(),
+            registry: fields[2].to_string(),
+            allocated: parse_date(fields[3]),
+            as_name: fields[4].to_string(),
+        };
+
+        results.push(result);
     }
 
-    let fields: Vec<&str> = records[0].split("|").map(str::trim).collect();
-    let as_number: u32 = try!(fields[0].parse().map_err(|_| "u32 parse error"));
-
-    let result = CymruASN {
-        as_number: as_number,
-        country_code: fields[1].to_string(),
-        registry: fields[2].to_string(),
-        allocated: parse_date(fields[3]),
-        as_name: fields[4].to_string(),
-    };
-
-    Ok(result)
+    results
 }
 
 
-/// Parse Cymru's Origin query result string into a struct
+/// Parse Cymru's Origin query result string into a structs
 ///
 /// Sample DNS TXT response we try to parse:
 ///
@@ -140,23 +172,27 @@ fn parse_cymru_asn(records: Vec<String>) -> Result<CymruASN, String> {
 ///
 /// taken from https://www.team-cymru.org/IP-ASN-mapping.html#dns
 ///
-fn parse_cymru_origin(records: Vec<String>) -> Result<CymruOrigin, String> {
-    if records.len() != 1 {
-        return Err("Invalid number of TXT records found".to_string());
+fn parse_cymru_origin(records: Vec<String>) -> Vec<CymruOrigin> {
+    let mut results = Vec::with_capacity(records.len());
+
+    for record in records {
+        let fields: Vec<&str> = record.split("|").map(str::trim).collect();
+        let as_number: u32 = match fields[0].parse() {
+            Err(_) => continue,
+            Ok(n) => n,
+        };
+
+        let result = CymruOrigin {
+            as_number: as_number,
+            bgp_prefix: fields[1].to_string(),
+            country_code: fields[2].to_string(),
+            registry: fields[3].to_string(),
+            allocated: parse_date(fields[4]),
+        };
+        results.push(result);
     }
 
-    let fields: Vec<&str> = records[0].split("|").map(str::trim).collect();
-    let as_number: u32 = try!(fields[0].parse().map_err(|_| "u32 parse error"));
-
-    let result = CymruOrigin {
-        as_number: as_number,
-        bgp_prefix: fields[1].to_string(),
-        country_code: fields[2].to_string(),
-        registry: fields[3].to_string(),
-        allocated: parse_date(fields[4]),
-    };
-
-    Ok(result)
+    results
 }
 
 
@@ -218,27 +254,42 @@ mod tests {
     fn test_parse_cymru_asn() {
         use super::{CymruASN, parse_cymru_asn, parse_date};
         let vec = vec!["23028 | US | arin | 2002-01-04 | TEAMCYMRU - SAUNET".to_string()];
-        let parsed: CymruASN = parse_cymru_asn(vec).unwrap();
-        let valid = CymruASN {
-            as_number: 23028,
-            country_code: "US".to_string(),
-            registry: "arin".to_string(),
-            allocated: parse_date("2002-01-04"),
-            as_name: "TEAMCYMRU - SAUNET".to_string(),
-        };
-        assert_eq!(parsed, valid)
+        let results: Vec<CymruASN> = parse_cymru_asn(vec);
+        assert_eq!(results.len(), 1);
+        let first = results.first().unwrap();
+        assert_eq!(first.as_number, 23028);
+        assert_eq!(first.country_code, "US");
+        assert_eq!(first.registry, "arin");
+        assert_eq!(first.allocated, parse_date("2002-01-04"));
+        assert_eq!(first.as_name, "TEAMCYMRU - SAUNET");
+    }
+
+    #[test]
+    fn test_parse_cymru_asn_empty() {
+        use super::{CymruASN, parse_cymru_asn};
+        let results: Vec<CymruASN> = parse_cymru_asn(vec!["".to_string()]);
+        assert_eq!(results.len(), 0);
     }
 
     #[test]
     fn test_parse_cymru_origin() {
         use super::{CymruOrigin, parse_cymru_origin, parse_date};
         let vec = vec!["23028 | 216.90.108.0/24 | US | arin | 1998-09-25".to_string()];
-        let parsed: CymruOrigin = parse_cymru_origin(vec).unwrap();
-        assert_eq!(parsed.as_number, 23028);
-        assert_eq!(parsed.bgp_prefix, "216.90.108.0/24");
-        assert_eq!(parsed.country_code, "US");
-        assert_eq!(parsed.registry, "arin");
-        assert_eq!(parsed.allocated, parse_date("1998-09-25"));
+        let results: Vec<CymruOrigin> = parse_cymru_origin(vec);
+        assert_eq!(results.len(), 1);
+        let first = results.first().unwrap();
+        assert_eq!(first.as_number, 23028);
+        assert_eq!(first.bgp_prefix, "216.90.108.0/24");
+        assert_eq!(first.country_code, "US");
+        assert_eq!(first.registry, "arin");
+        assert_eq!(first.allocated, parse_date("1998-09-25"));
+    }
+
+    #[test]
+    fn test_parse_cymru_origin_empty() {
+        use super::{CymruOrigin, parse_cymru_origin};
+        let results: Vec<CymruOrigin> = parse_cymru_origin(vec!["".to_string()]);
+        assert_eq!(results.len(), 0);
     }
 
 }
