7720bcc040234460e315bed1fd926b70b8af7a81
Ossi Herrala <oherrala@iki.fi>
2017-03-14 11:29:28 +0200

    Add simple whois example.
    Example can be run by `cargo run -q --example whois <IP address>`

---
+++ examples/whois.rs
@@ -0,0 +1,12 @@
+extern crate cymrust;
+
+use std::env;
+use std::net::IpAddr;
+
+fn main() {
+    let first_arg = env::args().nth(1).unwrap();
+    let ip: IpAddr = first_arg.parse().unwrap();
+
+    let cymru = cymrust::cymru_ip2asn(ip);
+    println!("{:#?}", cymru)
+}
